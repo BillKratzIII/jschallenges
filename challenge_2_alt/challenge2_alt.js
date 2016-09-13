403b3a3895bf26024a3c7b6db48647876881e372
BillKratzIII <wkratz3@gmail.com>
2016-09-13 14:59:08 -0400

    9/13/16 2:58PM completed challenge two alt

--- challenge_2_alt/challenge2_alt.js
+++ challenge_2_alt/challenge2_alt.js
@@ -1,35 +1,73 @@
-/**
- * Hockey is the game. Make it happen.
- *
- * If you want to turn it into some other sport, have at it.
- *
- * Anyway, I have given you a basic HTML structure for a 
- * BATTLE OF THE SPORTS GAME THING between these two rivals, and you
- * should make the page do what it needs to do, using your knowledge
- * of JS, HTML, CSS, and... sports.
- *
- * Here's what this 'game' should do:
- * 
- * 1. Clicking a "SHOOT" button attempt to score against the opposing team.
- *   - shots have a random chance of succeeding or failing
- *   - number of shots taken should increase every click on the "SHOOT" button
- *   - number of hits obviously only increases when the shot is successful
- * 
- * 2. Clicking the "RESET" button resets all the shot and score counters and
- * adds 1 to the number of resets
- *
- * 3. Any time a team shoots and scores change the background color of
- *    page to that teams color
- *
- * OPTIONAL STUFF:
- * - add logos of the two rivals below their name
- * - make the page just look better
- * - play a sound when someone clicks the "Shoot" button. You'll need to read about the <audio> element
- *   and how to use it in JS. You will also need to download a sound bite
- */
+/* William Kratz
+ * 9/13/16
+*/
+
+
+ 
 
  $(function(){
+ 	//variables used to track resets, team shots and goals.
+ 	var shootButtonOne = $('#teamone-shoot');
+	var shootButtonTwo = $('#teamtwo-shoot');
+	var resetButton = $('#reset')
+	var counter1 = 0;
+	var counter2 = 0;
+	var resetCounter = 0;
+	var teamOneHits = 0;
+	var teamTwoHits = 0;
+ 	
+ 	//when clicked, the counter goes up by one. If the result of the random number is 1 then the goal goes up by one and the back
+ 	//ground color changes to the team's color.
+	shootButtonOne.click(function(){ 		
+ 		counter1++;
+ 		$('#teamone-numshots').html(counter1);
+ 		var result = getRandomIntInclusive();
+ 		if(result==1){
+ 			$('body').css("background-color","red");
+ 			alert("GOAAAALLLLL!!!!");
+ 			teamOneHits++;
+ 			$('#teamone-numhits').html(teamOneHits);
+ 		}else{
+ 			$('body').css("background-color","white");
+ 		}
+ 	});
 
+	//same functionality as team one's shoot button
+ 	shootButtonTwo.click(function(){ 		
+ 		counter2++;
+ 		$('#teamtwo-numshots').html(counter2);
+ 		var result = getRandomIntInclusive();
+ 		if(result==1){
+ 			$('body').css("background-color","blue");
+ 			alert("GOAAAALLLLL!!!!");
+ 			teamTwoHits++;
+ 			$('#teamtwo-numhits').html(teamTwoHits);
+ 		}else{
+ 			$('body').css("background-color","white");
+ 		}
+ 	});
+
+ 	//when clicked, resets the counters for team scoring and add's one to the reset counter.
+ 	resetButton.click(function(){
+ 		$('body').css("background-color","white");
+ 		counter1 = 0;
+ 		counter2 = 0;
+ 		teamOneHits = 0;
+ 		teamTwoHits = 0;
+ 		resetCounter++;
+ 		$('#num-resets').html(resetCounter);
+ 		$('#teamone-numshots').html(counter1);
+ 		$('#teamone-numhits').html(teamOneHits);
+ 		$('#teamtwo-numshots').html(counter2);
+ 		$('#teamtwo-numhits').html(teamTwoHits);
+ 	});
+
+ 	//function returns a random number either 0 or 1.
+	function getRandomIntInclusive() {
+ 		min = Math.ceil(0);
+ 		max = Math.floor(1);
+  		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
 
 
  })
